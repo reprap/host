de98b8e5b3c5c113c31386af1cba346bf60e8935
Rhys Jones <r.o.jones@bath.ac.uk>
2011-05-20 13:41:37 +0000

    Fixing percent infill bug

--- src/org/reprap/devices/GenericExtruder.java
+++ src/org/reprap/devices/GenericExtruder.java
@@ -411,10 +411,7 @@ public abstract class GenericExtruder implements Extruder
 			extrusionTemp = Preferences.loadGlobalDouble(prefName + "ExtrusionTemp(C)");
 			extrusionSize = Preferences.loadGlobalDouble(prefName + "ExtrusionSize(mm)");
 			extrusionHeight = Preferences.loadGlobalDouble(prefName + "ExtrusionHeight(mm)");		
-			if (Preferences.loadGlobalDouble(prefName + "ExtrusionInfillWidth(mm)")==-1)
-				extrusionInfillWidth =  extrusionSize/Preferences.loadGlobalDouble(prefName + "ExtrusionPercentInfill(0..1)");
-			else
-				extrusionInfillWidth =  Preferences.loadGlobalDouble(prefName + "ExtrusionInfillWidth(mm)");
+			extrusionInfillWidth =  Preferences.loadGlobalDouble(prefName + "ExtrusionInfillWidth(mm)");
 			lowerFineLayers = 2; //Preferences.loadGlobalInt(prefName + "LowerFineLayers(0...)");
 			upperFineLayers = 2; //Preferences.loadGlobalInt(prefName + "UpperFineLayers(0...)");
 			extrusionBroadWidth = Preferences.loadGlobalDouble(prefName + "ExtrusionBroadWidth(mm)");		
